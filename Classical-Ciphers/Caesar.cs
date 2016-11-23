e077b96f1e71b4cd6fe49e2846cdaa9f4bbec40e
vmdharan <rexar7@gmail.com>
2016-11-23 14:52:29 +1100

    Allow selecting a file
    - Added function to allow selection of a file using a dialog and showing
    the file size in the program.
    - Created placeholder code for encoding and decoding using the Caesar
    Cipher.

---
+++ Classical-Ciphers/Caesar.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classical_Ciphers
+{
+    // Implement the Caesar Cipher.
+    class Caesar
+    {
+        private int shiftValue = 3;
+
+        // Default constructor.
+        public Caesar()
+        {
+            // No shift value provided, so use the default shift value.
+        }
+
+        // Alternative constructor, when a specific shift value is provided.
+        public Caesar(int shift)
+        {
+            // Do sanity checking of the shift value, and clamp it to the English 
+            // alphabet length of 26.
+            if(shift > 25)
+            {
+                shiftValue = 25;
+            }
+            else if(shift < 0)
+            {
+                shiftValue = 0;
+            }
+            else
+            {
+                // Save the shift value to use with the Cipher.
+                shiftValue = shift;
+            }
+        }
+
+        // Encode method.
+        public void encode()
+        {
+
+        }
+
+        // Decode method.
+        public void decode()
+        {
+
+        }
+    }
+}
--- Classical-Ciphers/MainWindow.xaml.cs
+++ Classical-Ciphers/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,46 @@ namespace Classical_Ciphers
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Buffer to store the data from the input file.
+        private byte[] inputData;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, RoutedEventArgs e)
+        {
+            // Show the 'open file' dialog.
+            var dialog_SelectFile = new System.Windows.Forms.OpenFileDialog();
+            var retval = dialog_SelectFile.ShowDialog();
+
+            try
+            {
+                switch(retval)
+                {
+                    case System.Windows.Forms.DialogResult.OK:
+                        // Get the picked file.
+                        var pickedFile = dialog_SelectFile.FileName;
+                        tbPickedFile.Text = pickedFile;
+                        tbPickedFile.ToolTip = pickedFile;
+
+                        // Read data from the picked file into the buffer.
+                        inputData = File.ReadAllBytes(tbPickedFile.Text);
+
+                        // Get the file size
+                        lblFileSizeBytes.Content = inputData.Length.ToString();
+                        break;
+
+                    case System.Windows.Forms.DialogResult.Cancel:
+                    default:
+                        break;
+                }
+            }
+            catch (Exception ex1)
+            {
+                MessageBox.Show(ex1.Message);
+            }
         }
     }
 }
