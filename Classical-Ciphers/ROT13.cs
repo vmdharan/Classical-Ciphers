ca1b77a50b076fdd6d76ab4c707a6f76b034b642
vmdharan <rexar7@gmail.com>
2016-11-24 16:25:40 +1100

    Added ROT13
    Added ROT13 which is a special case of Caesar ciphers with a shift value
    of 13.

---
+++ Classical-Ciphers/ROT13.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classical_Ciphers
+{
+    class ROT13 : Caesar
+    {
+        public ROT13()
+        {
+            shiftValue = 13;
+        }
+    }
+}
